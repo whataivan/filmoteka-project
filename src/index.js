cb1eac0251f20ac7c5effb2ecc4a8e92b21ed975
Aleksandr Raskulov <alexandr1991r@gmail.com>
2022-07-06 17:01:27 +0300

    library-fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-
 import { fetchTrends, fetchByName, fetchGenres } from './js/api/fetchApi';
 import { paginationMarkup } from './js/pagination';
 let findErr = document.querySelector('.form-text');
 
-
 let obj1 = {};
 let forLocalStor;
 const urlImg = 'https://image.tmdb.org/t/p/w500';
+const galleryItem = document.querySelector('.gallery');
+const form = document.querySelector('.form');
 fetchGenres().then(data => {
-  
   data.genres.map(el => {
     obj1[el.id] = el.name;
     localStorage.setItem('genres', JSON.stringify(obj1));
@@ -20,25 +19,19 @@ fetchTrends().then(res => {
   localStorage.setItem('response', JSON.stringify(res.results));
 
   paginationMarkup(res.page, res.total_pages);
-
-
 });
 
-const form = document.querySelector('.form');
 form.addEventListener('submit', onSubmit);
 
 function onSubmit(evt) {
   evt.preventDefault();
   const query = evt.currentTarget.name.value.trim();
   if (!query) {
-
-
-    findErr.classList.remove('visually-hidden')
+    findErr.classList.remove('visually-hidden');
     setTimeout(() => {
       findErr.classList.add('visually-hidden');
-    }, 3000)
-    return
-
+    }, 3000);
+    return;
   }
 
   fetchByName(query)
@@ -56,32 +49,24 @@ function onSubmit(evt) {
       markUpForGallery(res.results);
 
       paginationMarkup(res.page, res.total_pages, query);
-
     })
     .catch(err => {
       console.log(err);
     });
 }
-const galleryItem = document.querySelector('.gallery');
-
 
 function markUpForGallery(arr) {
-
   galleryItem.innerHTML = '';
   let a = arr.reduce(
     (acc, el) =>
-      (acc += `<li class="gallery__item">
-
-
-        <a class="gallery__link" href="#">
-          <img id='${el.id}'
+      (acc += `<li id='${el.id}' class="gallery__item">
+      
+          <img 
             class="gallery__img"
             src='${
               el.poster_path
                 ? urlImg + el.poster_path
-
                 : 'https://s1.hostingkartinok.com/uploads/images/2022/07/40ceaea2e22257d2a139ca5a0c0b8ba9.jpg'
-
             }'
             alt="${el.original_title}"
             
@@ -107,7 +92,7 @@ function markUpForGallery(arr) {
 
                 </div>
                 </div>
-                </a>
+                
                 </li>`),
 
     ``
@@ -115,39 +100,5 @@ function markUpForGallery(arr) {
   galleryItem.insertAdjacentHTML('beforeend', a);
   //<span class="gallery-text__rating">${el.vote_average}</span>
 }
-function markUpForLibrary(arr) {
-  let a = arr.reduce(
-    (acc, el) =>
-      (acc += `<li class="gallery__item">
-        <a class="gallery__link" href="#">
-          <img
-            class="gallery__img"
-            src='${urlImg}${el.poster_path}'
-            alt=""
-            width = '280'
-          />
-          <div class="gallery-text">
-            <p class="gallery-text__title">${el.original_title}</p>
-            <div class="gallery-text__info">
-              <p class="gallery-text__genre"> ${
-                el.genre_ids.map(gen => {
-                  return (gen = obj1[gen]);
-                }).length > 3
-                  ? el.genre_ids
-                      .map(gen => {
-                        return (gen = ' ' + obj1[gen]);
-                      })
-                      .slice(0, 2) + ', Other '
-                  : el.genre_ids.map(gen => {
-                      return (gen = ' ' + obj1[gen]);
-                    })
-              } | ${el.release_date.slice(0, 4)}</p>
-        <span class="gallery-text__rating">${el.vote_average}</span>
-            </div>
-          </div>
-        </a>
-      </li>`),
-    ''
-  );
-  galleryItem.insertAdjacentHTML('beforeend', a);
-}
+
+export { markUpForGallery };
---
+++ src/js/markUp/markUpforLibrary.js
@@ -0,0 +1,40 @@
+const urlImg = 'https://image.tmdb.org/t/p/w500';
+let obj1 = {};
+const galleryItem = document.querySelector('.gallery');
+
+export function markUpForLibrary(arr) {
+  let a = arr.reduce(
+    (acc, el) =>
+      (acc += `<li id="${el.id}" class="gallery__item">
+        <a class="gallery__link" href="#">
+          <img
+            class="gallery__img"
+            src='${urlImg}${el.poster_path}'
+            alt=""
+            width = '280'
+          />
+          <div class="gallery-text">
+            <p class="gallery-text__title">${el.original_title}</p>
+            <div class="gallery-text__info">
+              <p class="gallery-text__genre"> ${
+                el.genre_ids.map(gen => {
+                  return (gen = obj1[gen]);
+                }).length > 3
+                  ? el.genre_ids
+                      .map(gen => {
+                        return (gen = ' ' + obj1[gen]);
+                      })
+                      .slice(0, 2) + ', Other '
+                  : el.genre_ids.map(gen => {
+                      return (gen = ' ' + obj1[gen]);
+                    })
+              } | ${el.release_date.slice(0, 4)}</p>
+        <span class="gallery-text__rating">${el.vote_average}</span>
+            </div>
+          </div>
+        </a>
+      </li>`),
+    ''
+  );
+  galleryItem.innerHTML = a;
+}
