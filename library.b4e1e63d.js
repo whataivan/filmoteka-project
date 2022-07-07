93fb8f6eeb7596e5269c3028a716190480cfdc72
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-07-07 14:35:02 +0000

    Deploying to gh-pages from @ Jasper935/filmoteka-project@5d885f9b6fce13e5e5f2dfc1ef0ad129aeed4e13 🚀

---
+++ library.b4e1e63d.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".button-open"),o=document.querySelector(".backdrop-team"),c=document.querySelector(".modal-group__close-btn");e.addEventListener("click",(function(e){console.log(e),o.classList.remove("backdrop-hidden")})),c.addEventListener("click",(function(e){console.log(e),o.classList.add("backdrop-hidden")}));
+//# sourceMappingURL=library.b4e1e63d.js.map
