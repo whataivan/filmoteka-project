008f54f0955e1b41b49e2516637f10791ac4eabc
Aleksandr Raskulov <alexandr1991r@gmail.com>
2022-07-09 00:14:50 +0300

    fix

--- src/js/library.js
+++ src/js/library.js
@@ -19,10 +19,9 @@ Notiflix.Notify.init({
   },
 });
 
-
 const watched = document.querySelector('.library__button--watched');
 const queue = document.querySelector('.library__button--queue');
-let elementForModal;//елемент открытой картинки
+let elementForModal;
 onClickWatched(); //dont touch
 
 let arrForQueue = [];
@@ -41,33 +40,17 @@ let deleteBtn;
 let addToQueue;
 //=============================
 gallery.addEventListener('click', onClick);
-// function checkAddBtn() {
-//   if (watched.classList.contains('active')) {
-//    const res = JSON.parse(localStorage.getItem('watched')).some(el=>el.id===elementForModal.id)
-//    console.log(res);
-//    if (res) {
-//     addToQueue.setAttribute('disabled', true)
-//    }
-
-//   } else if (queue.classList.contains('active')) {
-//     checkDeleteBtn('queue');
-
-//   }
-// }
 
 function onClick(event) {
   if (!event.target.closest('li')) {
     return;
   } else {
-    
-    let response = JSON.parse(localStorage.getItem('response'));
+    let response = JSON.parse(localStorage.getItem('watched'));
     backdrop.classList.remove('is-hidden');
     const currentId = event.target.closest('li').id;
     elementForModal = response.find(el => String(el.id) === currentId);
-    
-
+    console.log(elementForModal);
     if (watched.classList.contains('active')) {
-
       markUpModalLib(elementForModal, 'REMOVE FROM LIST', 'add to queue');
     } else {
       markUpModalLib(elementForModal, 'REMOVE FROM LIST', 'add to watched');
@@ -81,49 +64,39 @@ function onClick(event) {
 function onClickDelete() {
   if (watched.classList.contains('active')) {
     checkDeleteBtn('watched');
-
   } else if (queue.classList.contains('active')) {
     checkDeleteBtn('queue');
-
   }
-
 }
 function onClickAddtBtn() {
-  
-  if (queue.classList.contains('active')) {
-    let res = arrForWatched.filter(el => el.id !== elementForModal.id)
-    arrForWatched = res
-    arrForWatched.push(elementForModal)
-    localStorage.setItem('watched', JSON.stringify(arrForWatched))
-    addToQueue.setAttribute('disabled', true)
-    addToQueue.classList.add('inactive')
-
-  } else if (watched.classList.contains('active')) {
-    let res = arrForQueue.filter(el => el.id !== elementForModal.id)
-    arrForQueue = res
-    arrForQueue.push(elementForModal)
-    localStorage.setItem('queue', JSON.stringify(arrForQueue))
-    addToQueue.setAttribute('disabled', true)
-    addToQueue.classList.add('inactive')
+  if (watched.classList.contains('active')) {
+    let res = arrForWatched.filter(el => el.id !== forLocalStorage.id);
+    arrForWatched = res;
+    arrForWatched.push(forLocalStorage);
+    localStorage.setItem('watched', JSON.stringify(arrForWatched));
+  } else if (queue.classList.contains('active')) {
+    checkDeleteBtn('queue');
   }
 }
+// function checkAddBtn{
 
+// }
 function checkDeleteBtn(name) {
-
   if (deleteBtn.textContent === 'REMOVE FROM LIST') {
-
     Notiflix.Notify.success('Film was removed');
     deleteBtn.classList.add('visually-hidden');
     let resFromStorage = JSON.parse(localStorage.getItem(name));
 
-    let resToStorage = resFromStorage.filter(el => el.id !== elementForModal.id);
+    let resToStorage = resFromStorage.filter(
+      el => el.id !== elementForModal.id
+    );
 
     localStorage.setItem(name, JSON.stringify(resToStorage));
     // return 'ADD';
     if (name === 'watched') {
-      onClickWatched()
+      onClickWatched();
     } else if (name === 'queue') {
-      onClickQueue()
+      onClickQueue();
     }
   }
   return;
@@ -137,8 +110,8 @@ function onClickWatched() {
   const item = JSON.parse(localStorage.getItem('watched'));
   if (item) {
     markUpForLibrary(item);
-    firstPaginationCall('watched');//dont touch==============
-
+    firstPaginationCall('watched'); //dont touch==============
+    // markUpModalLib()
   } else {
     markUpForLibrary([]);
   }
@@ -149,7 +122,7 @@ function onClickQueue() {
   watched.classList.remove('active');
   if (item) {
     markUpForLibrary(item);
-    firstPaginationCall('queue');//dont touch===========
+    firstPaginationCall('queue'); //dont touch===========
   } else {
     markUpForLibrary([]);
   }
