73610d1f5eb5c0b096ce2ddc940deeb48baebbca
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-07-10 18:52:41 +0000

    Deploying to gh-pages from @ Jasper935/filmoteka-project@66abc00881f29d0a7576c77177b7f79639d2ee23 🚀

--- library.e3fd9fe6.js
+++ library.e3fd9fe6.js
@@ -1,2 +1,2 @@
 !function(){var e=document.querySelector(".back-to-top");document.addEventListener("scroll",(function(){(document.documentElement||document.body).scrollTop>500?e.classList.remove("hidden"):e.classList.add("hidden")})),e.addEventListener("click",(function(){document.body.scrollIntoView({behavior:"smooth"})}))}();
-//# sourceMappingURL=index.47f23baf.js.map
+//# sourceMappingURL=library.e3fd9fe6.js.map
