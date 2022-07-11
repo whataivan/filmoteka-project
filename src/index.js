627b4e1185d562d11109baa2251e3cac0c4f8ee1
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-07-11 10:39:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -60,6 +60,7 @@ function onSubmit(evt) {
           setTimeout(() => {
             findErr.classList.add('visually-hidden');
           }, 3000);
+          paginationMarkup(res.page, res.total_pages, query);
           removePagination()
           return;
         }
